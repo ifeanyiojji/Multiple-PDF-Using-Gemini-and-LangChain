534fd9cc598353fbc5112b69711da79887e54799
ifeanyi_ojji <shedrachifeanyi725@gmail.com>
2025-04-25 20:07:03 +0100

    Preparing fot deployment

--- stapp.py
+++ stapp.py
@@ -67,7 +67,7 @@ def user_input(user_question):
     docs = new_db.similarity_search(user_question)
     chain = get_conversational_chain()
     response = chain.invoke({"context": docs, "question": user_question})
-    st.write("Response:", response['output_text'])
+    st.write("Response:", response)
 
 
 def main():
